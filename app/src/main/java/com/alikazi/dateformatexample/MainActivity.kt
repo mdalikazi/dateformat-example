d14bdf2311098e9091e6d7a4271c1d559cd89933
Ali Kazi <mdalikazi@gmail.com>
2018-05-01 18:04:40 +1000

    Clean

--- app/src/main/java/com/alikazi/dateformatexample/MainActivity.kt
+++ app/src/main/java/com/alikazi/dateformatexample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.alikazi.dateformatexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.View
 import android.widget.*
 import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         const val TIME_ZONE_GMT_0 = "GMT+00:00"
     }
 
+    private var mDefaultLocalePosition: Int = 0
     private var mDateCalendar: Calendar = Calendar.getInstance()
     private var mSelectedLocale: Locale = Locale.getDefault()
     private var mSortedLocaleList: List<Locale> = ArrayList()
@@ -30,6 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         val spinner: Spinner = findViewById(R.id.spinner)
         spinner.adapter = getSpinnerAdapter()
+        spinner.setSelection(mDefaultLocalePosition)
         spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
@@ -65,19 +69,18 @@ class MainActivity : AppCompatActivity() {
         var shortTime = getTimeFormat(DateFormat.SHORT)
         var defaultTime = getTimeFormat(DateFormat.DEFAULT)
 
-        var fullDateTime = getDateTimeFormat(DateFormat.FULL, DateFormat.SHORT)
+        var fullDateShortTime = getDateTimeFormat(DateFormat.FULL, DateFormat.SHORT)
+        var mediumDateMediumTime = getDateTimeFormat(DateFormat.MEDIUM, DateFormat.MEDIUM)
 
-//        var timezone = DateFormat.getTimeInstance(DateFormat.TIMEZONE_FIELD).format(mDateCalendar.time)
-//        var dayOfWeek = DateFormat.getTimeInstance(DateFormat.DAY_OF_WEEK_FIELD).format(mDateCalendar.time)
-//        var amPm = DateFormat.getTimeInstance(DateFormat.AM_PM_FIELD).format(mDateCalendar.time)
-//        var hourOfDay0 = DateFormat.getTimeInstance(DateFormat.HOUR_OF_DAY0_FIELD).format(mDateCalendar.time)
-//        var hourOfDay1 = DateFormat.getTimeInstance(DateFormat.HOUR_OF_DAY1_FIELD).format(mDateCalendar.time)
-//        var minute = DateFormat.getTimeInstance(DateFormat.MINUTE_FIELD).format(mDateCalendar.time)
-//        var second = DateFormat.getTimeInstance(DateFormat.SECOND_FIELD).format(mDateCalendar.time)
-//        var millisecond = DateFormat.getTimeInstance(DateFormat.MILLISECOND_FIELD).format(mDateCalendar.time)
+        var dateTime = DateUtils.formatDateTime(this, mDateCalendar.timeInMillis, DateUtils.FORMAT_SHOW_DATE + DateUtils.FORMAT_SHOW_TIME)
+        var weekdayMonthDate = DateUtils.formatDateTime(this, mDateCalendar.timeInMillis, (DateUtils.FORMAT_SHOW_WEEKDAY + DateUtils.FORMAT_ABBREV_WEEKDAY + DateUtils.FORMAT_ABBREV_MONTH + DateUtils.FORMAT_SHOW_DATE))
+        var weekdayMonthDateYear = DateUtils.formatDateRange(this, mDateCalendar.timeInMillis, mDateCalendar.timeInMillis + 1000000000, (DateUtils.FORMAT_SHOW_WEEKDAY + DateUtils.FORMAT_ABBREV_WEEKDAY + DateUtils.FORMAT_ABBREV_MONTH + DateUtils.FORMAT_SHOW_DATE + DateUtils.FORMAT_SHOW_YEAR))
+        var simpleDateFormat = SimpleDateFormat("EEE MMM dd", mSelectedLocale)
+        var withSimpleDateFormat = simpleDateFormat.format(mDateCalendar.time)
 
         val textView: TextView = findViewById(R.id.dateformat_test_text_view)
         textView.text =
+                "Dates:" + "\n" + "\n" +
                 "date: " + date + "\n" + "\n" +
                 "month: " + month + "\n" + "\n" +
                 "year: " + year + "\n" + "\n" +
@@ -87,20 +90,22 @@ class MainActivity : AppCompatActivity() {
                 "shortDate: " + shortDate + "\n" + "\n" +
                 "defaultDate: " + defaultDate + "\n" + "\n" + "\n" +
 
+                "Times:" + "\n" + "\n" +
                 "fullTime: " + fullTime + "\n" + "\n" +
                 "longTime: " + longTime + "\n" + "\n" +
                 "mediumTime: " + mediumTime + "\n" + "\n" +
                 "shortTime: " + shortTime + "\n" + "\n" +
-                "defaultTime: " + defaultTime + "\n" + "\n" +
+                "defaultTime: " + defaultTime + "\n" + "\n" + "\n" +
 
-                "fullDateTime: " + fullDateTime + "\n"
-//                "dayOfWeek: " + dayOfWeek + "\n" +
-//                "amPm: " + amPm + "\n" +
-//                "hourOfDay0: " + hourOfDay0 + "\n" +
-//                "hourOfDay1: " + hourOfDay1 + "\n" +
-//                "minute: " + minute + "\n" +
-//                "second: " + second + "\n" +
-//                "millisecond: " + millisecond
+                "DateTimes:" + "\n" + "\n" +
+                "fullDateTime: " + fullDateShortTime + "\n" + "\n" +
+                "mediumDateTime: " + mediumDateMediumTime + "\n" + "\n" + "\n" +
+
+                "DateUtils:" + "\n" + "\n" +
+                "dateTime: " + dateTime + "\n" + "\n" +
+                "weekdayMonthDate: " + weekdayMonthDate + "\n" + "\n" +
+                "weekdayMonthDateYear: " + weekdayMonthDateYear + "\n" + "\n" +
+                "withSimpleDateFormat: " + withSimpleDateFormat + "\n" + "\n"
     }
 
     private fun getDateFormat(dateStyle: Int) : String {
@@ -136,8 +141,11 @@ class MainActivity : AppCompatActivity() {
     private fun getSpinnerAdapter() : ArrayAdapter<String> {
         mSortedLocaleList = DateFormat.getAvailableLocales().asList().sortedWith(compareBy { it.displayName })
         val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(this, R.layout.array_adapter_text_item)
-        for(locale: Locale in mSortedLocaleList) {
-            arrayAdapter.add(locale.displayName)
+        for(locale in mSortedLocaleList.withIndex()) {
+            arrayAdapter.add(locale.value.displayName)
+            if (locale.value == Locale.getDefault()) {
+                mDefaultLocalePosition = locale.index
+            }
         }
 
         return arrayAdapter
